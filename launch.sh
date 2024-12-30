17a13c37194e4333b59a8fbe9a4b45e94b4fce92
brettp <brettperry7@gmail.com>
2024-12-30 07:31:52 -0600

    added gitignore and revised launch script

--- launch.sh
+++ launch.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
+#must be ran from project root directory
 
+
+#check that the database exists
+echo "You will need to enter MySQL password here."
+result=$(mysql -u root -p -e "SHOW DATABASES LIKE 'BlogApplication';") 
+if [[ $result == *"BlogApplication"* ]]; then 
+	echo "The database exists." 
+else 
+	echo "The database does not exist." 
+fi
+
+#create virtual environment
+if [ -d "env/" ]; then 
+	echo "Directory exists." 
+else 
+	python3 -m venv env
+fi
+
+#start virtual environment
 source env/bin/activate
+#install requirements
 pip3 install -r requirements.txt
+#start the app
 uvicorn main:app --reload
