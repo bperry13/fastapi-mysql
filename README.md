72f533e8da273d48c4c00c7e8bb38b38b5a3659d
brettp <brettperry7@gmail.com>
2024-12-29 21:16:39 -0600

    app is connected to database 23:20

---
+++ README.md
@@ -0,0 +1,6 @@
+# Starter code for FastAPI + MySQL Back End
+## The dev environment is Ubuntu 22.04 with Python 3.10
+## To launch a dev environment (all commands from root directory)
+### If this is the first time then run ``` python3 -m venv env ``` to setup virtual env
+### After a virtual env is setup then run ``` ./launch.sh ``` to launch the app
+## To exit a dev environment go to the project root directory and run ``` deactivate ```
--- main.py
+++ main.py
@@ -25,9 +25,24 @@ def get_db():
         db.close() #don't want to keep the db open too long
 
 #create an annotation for our db for dependency injection
-db_dependency = Annotated(Session, Depends(get_db))
+db_dependency = Annotated[Session, Depends(get_db)]
 
+@app.post("/posts/", status_code=status.HTTP_201_CREATED)
+async def create_post(post: PostBase, db: db_dependency):
+    db_post = models.Post(title=post.title, content=post.content, user_id=post.user_id)
+    db.add(db_post)
+    db.commit()
 
+@app.post("/users/", status_code=status.HTTP_201_CREATED)
+async def create_user(user: UserBase, db: db_dependency):
+    db_user = models.User(username=user.username)
+    db.add(db_user)
+    db.commit()
 
-
+@app.get("/users/{user_id}", status_code=status.HTTP_200_OK)
+async def read_user(user_id:int, db: db_dependency):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
 
